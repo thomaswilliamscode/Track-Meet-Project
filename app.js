b88c84bc97942fd7c7fd7cacaff628237a623464
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2023-06-26 18:51:57 -0400

    Did all my tests on week3B to split the data properly, and then create a class out of the data, and then add that class to an object to hold everyones name and week3B scores.

--- app.js
+++ app.js
@@ -23,4 +23,60 @@ i will need to save this data to match up against new track meet scores in the f
 
 */
 
+// split the \n 
+let week3BSplit = week3B.split('\n');
+let newWeek3B = []
+for ( let array in week3BSplit ) {
+	let newWeek = week3BSplit[array].split('--')
+	newWeek3B.push(newWeek);
+}
+
+// create a class constructor 
+class Bloomer {
+	constructor (name, score) {
+		this.name = name;
+		this['week3B score'] = score;
+	}
+}
+
+// Give info to constructor
+let bloomer = [];
+
+// function to create bloomer instance for every person in track meet challenge. 
+function createBloomers () {
+	for (let i = 1; i < newWeek3B.length - 1; i++) {
+		bloomer[i - 1] = new Bloomer( newWeek3B[i][0], newWeek3B[i][1] )
+		
+	}
+}
+
+// runs the function
+createBloomers();
+
+// big list that holds all bloomers that were constructed
+const allBloomers = {
+	1: bloomer[0]
+};
+
+// function to add all bloomers to the the all Bloomers object
+function addToAllBloomers () {
+	for (let i = 0; i < bloomer.length; i++) {
+		allBloomers[i] = bloomer[i];
+	}
+}
+
+// runs the function 
+addToAllBloomers()
+
+
+// example of how to log info 
+console.log(allBloomers[1].name, allBloomers[1]['week3B score'])
+
+
+// if bloomer name is === bloomer name update that bloomers week scores
+if (allBloomers[1].name === 'Aidan Au') {
+	allBloomers[1]['week test'] = 'test'
+	console.log(allBloomers[1])
+}
+
 
