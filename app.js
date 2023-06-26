ce4af852b03a841bf4e72998894997711b3f07cf
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2023-06-26 16:37:33 -0400

    added weeklyScores .js file with weekly scores delted input because its no longer needed, and linked new .js file

---
+++ app.js
@@ -0,0 +1,26 @@
+/*	
+
+First we need an empty string to store everything in.
+
+They come in this format: 
+Thomas Williams--240
+Antwan Myles jr--155
+
+i will need to add track meet names, ---, and scores to the string.
+
+i will need to separate the names from the --
+
+i will need to separate the numbers from the --
+
+i will need to make sure that each name has the correct number associated with it.
+
+i will need to display this data in the way i want 
+
+i will need to save this data to match up against new track meet scores in the future. 
+
+
+
+
+*/
+
+
